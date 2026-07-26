02f56a681eec97c0412fb8c1b06b467180eb3d80
Drakkarrr <drakkar.alpha00@gmail.com>
2026-07-26 23:59:11 +0800

    Initialized project and added core dependencies

---
+++ src/main/java/com/store/drakkar/DrakkarApplication.java
@@ -0,0 +1,13 @@
+package com.store.drakkar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrakkarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrakkarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/store/drakkar/DrakkarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.drakkar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrakkarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
