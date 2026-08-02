ba0d857ce9282b51eb7cd8cd37b3fb4bf24596eb
Drakkarrr <drakkar.alpha00@gmail.com>
2026-08-02 21:31:50 +0800

    enable JPA Auditing Required for @CreatedDate / @LastModifiedDate to fire

--- src/main/java/com/store/DrakkarApplication.java
+++ src/main/java/com/store/DrakkarApplication.java
@@ -2,8 +2,10 @@ package com.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class DrakkarApplication {
 
     public static void main(String[] args) {
