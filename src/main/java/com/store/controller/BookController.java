03fde9c8222eaae9e62d59750a7b068378a8c49f
Drakkarrr <drakkar.alpha00@gmail.com>
2026-08-03 18:17:32 +0800

    created book controller completed for CRUD method

---
+++ src/main/java/com/store/controller/BookController.java
@@ -0,0 +1,47 @@
+package com.store.controller;
+
+import com.store.dto.BookRequest;
+import com.store.dto.BookResponse;
+import com.store.service.BookService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping ("/api/v1/books")
+@RequiredArgsConstructor
+public class BookController {
+
+    private final BookService bookService;
+
+    @PostMapping
+    public ResponseEntity<BookResponse> create(@Valid @RequestBody BookRequest request){
+        BookResponse response = bookService.create(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BookResponse>getById(@PathVariable Long id) {
+        return ResponseEntity.ok(bookService.getById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<BookResponse>> getAll() {
+        return ResponseEntity.ok(bookService.getAll());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<BookResponse> update(@PathVariable Long id, @Valid @RequestBody BookRequest request){
+        return ResponseEntity.ok(bookService.update(id, request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        bookService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
