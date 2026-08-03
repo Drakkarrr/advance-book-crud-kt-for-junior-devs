5095463fd4cae88414a43eae7d20a1f90e45d938
Drakkarrr <drakkar.alpha00@gmail.com>
2026-08-03 17:15:03 +0800

    created book service for all CRUD methods

---
+++ src/main/java/com/store/service/BookService.java
@@ -0,0 +1,14 @@
+package com.store.service;
+
+import com.store.dto.BookRequest;
+import com.store.dto.BookResponse;
+
+import java.util.List;
+
+public interface BookService {
+    BookResponse create(BookResponse request);
+    BookResponse getById(Long id);
+    List<BookResponse> getAll();
+    BookResponse update(Long id, BookRequest request);
+    void delete(Long id);
+}
